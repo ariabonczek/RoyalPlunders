454e1bc9d22d8796ea8bd5afd8de2f5aa9ab6324
Joshua Landman <jsl9284@rit.edu>
2016-09-21 14:56:12 -0400

    Implement Capture System 1. Introduce player interaction 2. 2+ Interactions to Capture 3. Set Jail Points Based on Complex Shapes

---
+++ Unity/Royal Plunders/Assets/Scripts/Interaction/PlayerInteraction.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInteraction : MonoBehaviour {
+
+    public float captureTime;
+    public Transform respawnPoint;
+
+    private bool vulnerable;
+
+    void Start()
+    {
+        captureTime = 0;
+        vulnerable = false;
+    }
+
+    public void interact(GameObject interactor)
+    {
+        if (!vulnerable)
+        {
+            vulnerable = true;
+            captureTime = Time.time + captureTime;
+        }
+        else
+        {
+            if (Time.time >= captureTime)
+            {
+                // TODO call HUD interactionss
+                transform.position = respawnPoint.position;
+            }
+        }
+    }
+
+    public string getTypeLabel()
+    {
+        return "Player";
+    }
+
+    public bool isInstant()
+    {
+        return false;
+    }
+}
---
+++ Unity/Royal Plunders/Assets/Scripts/SetJailPoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetJailPoint : MonoBehaviour 
+{
+    public Transform respawnPosition;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (other.GetComponent<PlayerInteraction>())
+            {
+                other.GetComponent<PlayerInteraction>().respawnPoint = respawnPosition;
+            }
+        }
+    }
+}
