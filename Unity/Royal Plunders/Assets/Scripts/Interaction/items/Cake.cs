0eaff14dc2fce505773ad666c112e5d8d49b113c
Joshua Landman <jsl9284@rit.edu>
2016-10-14 10:37:59 -0400

    Base cake

---
+++ Unity/Royal Plunders/Assets/Scripts/Interaction/items/Cake.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cake : MonoBehaviour, Iinteractable
+{
+    public float eatingTimer = 0;
+    public float eatTime;
+    public bool beingEaten;
+
+    public bool StartEating()
+    {
+        bool bRetVal = false;
+
+        if (!beingEaten)
+        {
+            beingEaten = true;
+            bRetVal = true;
+        }
+
+        return bRetVal;
+    }
+
+    public void interact(GameObject interactor)
+    {
+        if (!beingEaten)
+        {
+            if (StartEating())
+            {
+                EatCake();
+            }
+        }
+    }
+
+    public void EatCake()
+    {
+        while ( eatingTimer < eatTime )
+        {
+            // send guard ai data to change state
+            eatingTimer++;
+        }
+    }
+
+    public string getTypeLabel()
+    {
+        return "Cake";
+    }
+
+    public bool isInstant()
+    {
+        return false;
+    }
+}
