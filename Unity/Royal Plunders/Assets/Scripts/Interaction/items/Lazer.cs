49e3eb6f8cf3d05bc37118c803300bf09732c9ca
Joshua Landman <jsl9284@rit.edu>
2016-11-04 13:40:09 -0400

    Added lazers

---
+++ Unity/Royal Plunders/Assets/Scripts/Interaction/items/Lazer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lazer : MonoBehaviour , Iinteractable
+{
+    public GameObject alarmSystem;
+
+    public void interact(InteractionButton button, GameObject interactor)
+    {
+        alarmSystem.GetComponent<AlarmSystem>().TurnOnAlarm();
+    }
+
+    public string getTypeLabel()
+    {
+        return "Lazer";
+    }
+
+    public bool isInstant()
+    {
+        return true;
+    }
+}
