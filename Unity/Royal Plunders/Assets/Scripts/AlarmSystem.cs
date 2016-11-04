1d3189b3ac443969c791faf12d1f927648d27ac4
Joshua Landman <jsl9284@rit.edu>
2016-11-03 22:10:26 -0400

    Base alarm system 1. added alarm lights 2. working on alarm with ai 3. pushing now to avoid scene conflict 4. added prefabs 5. updated scenes for UI bug

---
+++ Unity/Royal Plunders/Assets/Scripts/AlarmSystem.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlarmSystem : MonoBehaviour {
+
+    public GameObject alarmLight;
+
+    public float alarmLightMaxIntensity;
+    public float alarmLightMinimumIntensity;
+    public float alarmLightCurrentIntensity;
+    public float alarmLightIncrement;
+    public bool alarmActive;
+
+    public GuardAITest.AIState alarmState;
+    public GuardAITest.AIState alarmOffState;
+
+    private float targetIntensity;
+    private bool alarmTurnedOn;
+
+    void Start()
+    {
+        alarmTurnedOn = false;
+        targetIntensity = alarmLightMaxIntensity;
+    }
+
+	// Update is called once per frame
+	void Update () 
+    {
+        if (alarmActive)
+        {
+            if (!alarmTurnedOn)
+            {
+                alarmLight.GetComponent<Light>().enabled = true;
+            
+                alarmLight.GetComponent<Light>().intensity = alarmLightCurrentIntensity;
+
+                for (int i = 0; i < GameManager.guardList.Length; i++)
+                {
+                    GameManager.guardList[i].GetComponent<GuardAITest>().myState = alarmState;
+                }
+
+                alarmTurnedOn = true;
+            }
+            
+            alarmLightCurrentIntensity = Mathf.Lerp(alarmLightCurrentIntensity, targetIntensity, alarmLightIncrement * Time.deltaTime);
+            CheckIntensity();
+        }
+        else
+        {
+            alarmTurnedOn = false;
+            alarmLight.GetComponent<Light>().enabled = false;
+            alarmLightCurrentIntensity = alarmLightMinimumIntensity;
+
+            for (int i = 0; i < GameManager.guardList.Length; i++)
+            {
+                GameManager.guardList[i].GetComponent<GuardAITest>().myState = alarmOffState;
+            }
+        }
+	}
+
+    public void CheckIntensity()
+    {
+        if (Mathf.Abs(targetIntensity - alarmLightCurrentIntensity) < alarmLightIncrement)
+        {
+            if (targetIntensity >= alarmLightMaxIntensity)
+            {
+                targetIntensity = alarmLightMinimumIntensity;
+            }
+            else
+            {
+                targetIntensity = alarmLightMaxIntensity;
+            }
+        }
+    }
+}
---
+++ Unity/Royal Plunders/Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public string GUARD_TAG;
+
+    public static GameObject[] guardList;
+
+    void Start()
+    {
+        guardList = GameObject.FindGameObjectsWithTag(GUARD_TAG);
+    }
+}
