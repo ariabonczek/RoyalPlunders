bf28ab3e2919fc8b6fe0d28526b553c9881c6274
Sam Handrick <sbh9278@rit.edu>
2016-12-05 18:57:59 -0800

    Add dynamic guard flashlights
    Guard flashlights are now configured specifically to the spot distance and view angle of the guard it belongs to.

---
+++ Unity/Royal Plunders/Assets/Scripts/GuardFlashlight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardFlashlight : MonoBehaviour {
+
+    GuardAITest guardScript;
+    Light myLight;
+
+	// Use this for initialization
+	void Start () {
+        guardScript = transform.parent.GetComponent<GuardAITest>();
+        myLight = GetComponent<Light>();
+
+        myLight.spotAngle =guardScript.angleOfView * 1.6f;
+        myLight.range = guardScript.PlayerSpotDistance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
