efedf2648742c64f99060e400406727b419e1d5b
Joshua Landman <jsl9284@rit.edu>
2016-12-13 01:53:22 -0500

    GUI key count

---
+++ Unity/Royal Plunders/Assets/Scripts/GUI/KeyText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class KeyText : MonoBehaviour {
+
+    public GameObject player;
+
+	// Update is called once per frame
+	void Update () 
+    {
+        InventoryManager invManager = player.GetComponent<InventoryManager>();
+        Text text = gameObject.GetComponent<Text>();
+
+        text.text = invManager.GetNumKeys().ToString();
+	}
+}
